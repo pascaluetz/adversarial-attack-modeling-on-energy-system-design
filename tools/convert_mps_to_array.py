c24c0086210b9e44e61d22d108015dd59c0d7f24
Pascal Uetz <pascal.uetz@gmail.com>
2022-07-19 15:38:19 +0200

    implementing tools

---
+++ tools/convert_mps_to_array.py
@@ -0,0 +1,251 @@
+"""
+This converter converts an LP problem from a *.mps file into the matrices for the following form:
+min c^T * x
+s.t.
+Ax <= b
+Hx == d
+
+Following assumptions to the problem in the *.mps file are made. If the problem violates one of these assumptions, the
+converter should be adjusted to it:
+- Minimization Problem
+- Objective is named "cost"
+- Equation names made from pyomo (because of sorting)
+- No greater than equation
+- Only lower bounds (>= 0) of variables and free bound variables
+"""
+
+
+from natsort import natsorted
+from scipy.sparse import csr_matrix
+
+
+# returns list of all variables and all variables with a lower bound of zero (>= 0)
+def getListOfVariablesAndBounds(model_data):
+    # area of bounds in mps file
+    start_line = model_data.index("BOUNDS\n") + 1
+    end_line = model_data.index("ENDATA\n")
+
+    # initialize containers
+    all_variables = []
+    bounded_variables = []
+
+    for index, line in enumerate(model_data[start_line:end_line]):
+        all_variables.append(line.split()[2])
+        # only get variables with a lower bound (>= 0)
+        sort = line.split()[0]
+        if sort == "LO":
+            bounded_variables.append(line.split()[2])
+        elif sort == "FR":
+            pass
+        else:
+            print(
+                "WARNING: Something went wrong with sorting the bounds to build variables list and bounding variables"
+                "list!"
+            )
+
+    # sort the list of variables
+    all_variables = natsorted(all_variables)
+    bounded_variables = natsorted(bounded_variables)
+
+    return all_variables, bounded_variables
+
+
+# get data to build matrices c, A and H
+def getListsOfcAH(model_data):
+    # area of the conditions in mps file
+    start_line = model_data.index("COLUMNS\n") + 1
+    end_line = model_data.index("RHS\n")
+
+    # initialize containers
+    c_cols = []
+    c_rows = []
+    c_values = []
+
+    A_cols = []
+    A_rows = []
+    A_values = []
+
+    H_cols = []
+    H_rows = []
+    H_values = []
+
+    # iterate through every condition
+    for index, line in enumerate(model_data[start_line:end_line]):
+        # sort the conditions for c (cost-function), A (upper bound) and H (equality)
+        sort = line.split()[1][2]
+        if sort == "s":
+            c_cols.append(line.split()[0])
+            c_rows.append(line.split()[1])
+            c_values.append(float(line.split()[2]))
+        elif sort == "u":
+            A_cols.append(line.split()[0])
+            A_rows.append(line.split()[1])
+            A_values.append(float(line.split()[2]))
+        elif sort == "e":
+            H_cols.append(line.split()[0])
+            H_rows.append(line.split()[1])
+            H_values.append(float(line.split()[2]))
+        elif sort == "l":
+            print("WARNING: Greater than (>=) equation will be ignored!")
+        else:
+            print("WARNING: Something went wrong with sorting the conditions to build the matrices!")
+
+    # combine containers for each matrix
+    c = [c_cols] + [c_rows] + [c_values]
+    A = [A_cols] + [A_rows] + [A_values]
+    H = [H_cols] + [H_rows] + [H_values]
+    return c, A, H
+
+
+# get data to build vectors b and d
+def getListOfbc(model_data):
+    # area of RHS (right-hand-side) in mps file
+    start_line = model_data.index("RHS\n") + 1
+    end_line = model_data.index("BOUNDS\n")
+
+    # initialize containers
+    b_rows = []
+    b_values = []
+
+    d_rows = []
+    d_values = []
+
+    # iterate through RHS definition
+    for index, line in enumerate(model_data[start_line:end_line]):
+        # sort the condition for b (<= equation) and d (== equation)
+        sort = line.split()[1][2]
+        if sort == "u":
+            b_rows.append(line.split()[1])
+            b_values.append(float(line.split()[2]))
+        elif sort == "e":
+            d_rows.append(line.split()[1])
+            d_values.append(float(line.split()[2]))
+        elif sort == "l":
+            print("WARNING: Greater than (>=) equation will be ignored!")
+        else:
+            print("WARNING: Something went wrong with sorting the RHS to build the vectors!")
+
+    # combine containers for each matrix
+    b = [b_rows] + [b_values]
+    d = [d_rows] + [d_values]
+
+    return b, d
+
+
+# modify A and b with variables with a lower bound of zero through adding conditions
+def getAbmodified(A_data, b_data, bounded_variables):
+    # unpack data
+    A_cols = A_data[0]
+    A_rows = A_data[1]
+    A_values = A_data[2]
+
+    b_rows = b_data[0]
+    b_values = b_data[1]
+
+    # iterate through every bounded variable
+    for index, bounded_variable in enumerate(bounded_variables):
+        # add for every bounded variable an equation
+        equation_name = "c_u_boundEQ(" + str(index) + ")_"
+        A_cols.append(bounded_variable)
+        A_rows.append(equation_name)
+        A_values.append(float(-1))
+
+        b_rows.append(equation_name)
+        b_values.append(float(0))
+
+    # combine containers
+    A = [A_cols] + [A_rows] + [A_values]
+    b = [b_rows] + [b_values]
+
+    return A, b
+
+
+# returns sparse matrix and description of rows
+def getDictAndMatrix(matrix_list, all_variables):
+    # unpack data
+    cols = matrix_list[0]
+    rows = matrix_list[1]
+    values = matrix_list[2]
+
+    # get a sorted list of all row names included in the matrix
+    # this is later used as a dictionary to assign a unique number to each equation name
+    row_names = natsorted(list(set(rows)))
+    row_names_as_dict = dict(zip(row_names, range(0, len(row_names))))
+
+    # all_variables as dict to speed up
+    all_variables_as_dict = dict(zip(all_variables, range(0, len(all_variables))))
+
+    # convert columns (=> variables) into numbers for further use with csr_matrix
+    # all_variables works as a dictionary and converts variable names into a unique number
+    cols_matrix = []
+    for col in cols:
+        cols_matrix.append(all_variables_as_dict[col])
+
+    # convert rows (=> equations) into numbers for further use with csr_matrix
+    # row_names works as a dictionary and converts equation names into a unique number
+    rows_matrix = []
+    for row in rows:
+        rows_matrix.append(row_names_as_dict[row])
+
+    # build sparse matrix (only filled sections are saved)
+    sparse_matrix = csr_matrix((values, (rows_matrix, cols_matrix)), shape=(len(row_names), len(all_variables)))
+
+    return row_names, sparse_matrix
+
+
+# returns vectors as a sparse matrix
+def getDictAndVectors(vector_list, row_names):
+    # unpack data
+    rows = vector_list[0]
+    values = vector_list[1]
+
+    # convert row_names to dict to speed up
+    row_names_as_dict = dict(zip(row_names, range(0, len(row_names))))
+
+    # convert rows (=> equation names) into numbers (same as for the associated matrix) and save them in rows_vector
+    # row_names works as a dictionary and converts equation names into a unique number
+    rows_vector = []
+    for row in rows:
+        rows_vector.append(row_names_as_dict[row])
+
+    # sort vector values according to rows_vector (so that it fits to the respective matrix)
+    # and return the values in the right order
+    values_new = [value for row, value in sorted(zip(rows_vector, values))]
+
+    # convert list into sparse matrix
+    values_new = csr_matrix(values_new).transpose()
+
+    return values_new
+
+
+# return all necessary data to fill model
+def script(model_data, bounded_variables_as_equations):
+    # get data out of the mps file
+    all_variables, bounded_variables = getListOfVariablesAndBounds(model_data)
+    c, A, H = getListsOfcAH(model_data)
+    b, d = getListOfbc(model_data)
+
+    if bounded_variables_as_equations:
+        # insert new equations for bounded variables into A and b
+        A, b = getAbmodified(A, b, bounded_variables)
+
+    # get sparse matrices and row names for each matrix
+    c_row_names, c = getDictAndMatrix(c, all_variables)
+    A_row_names, A = getDictAndMatrix(A, all_variables)
+    H_row_names, H = getDictAndMatrix(H, all_variables)
+
+    # get sparse vector (also csr_matrix) of each vector
+    # the row names (=> equation names) are defined through the row names of the respective matrix
+    b = getDictAndVectors(b, A_row_names)
+    d = getDictAndVectors(d, H_row_names)
+
+    return all_variables, bounded_variables, c_row_names, c, A_row_names, A, H_row_names, H, b, d
+
+
+# should be done in python file where the matrices are needed
+def run():
+    model_data = open("model.mps", "r").readlines()
+    script(model_data, bounded_variables_as_equations=True)
+
+
+# run()
---
+++ tools/time_series_converter.py
@@ -0,0 +1,60 @@
+"""
+Converts PV availability and demand year times series to the average of each hour over 365 days. The result is are 24h
+time series for a smaller model.
+"""
+
+import csv
+
+
+# extract digits from list
+def extractDigits(lst):
+    return [[el] for el in lst]
+
+
+# create empty arrays
+availability_pv = []
+DemandVal = []
+
+# open time series and add them to array
+with open("../time_series/TS_PVAvail.csv", "r") as file:
+    reader = csv.reader(file, delimiter="\n")
+    for row in reader:
+        availability_pv.append(float(row[0]))
+
+with open("../time_series/TS_Demand.csv", "r") as file:
+    reader = csv.reader(file, delimiter="\n")
+    for row in reader:
+        DemandVal.append(float(row[0]))
+
+# set ranges
+hours = range(24)
+days = range(365)
+
+# create empty arrays for 24h time series
+availability_pv_24h = []
+DemandVal_24h = []
+
+# loop over all days hourly and add the average to array
+for hour in hours:
+    value_availability = 0
+    value_demand = 0
+    # sum up all values of each hour for 365 days
+    for day in days:
+        value_availability += availability_pv[hour + day * 24]
+        value_demand += DemandVal[hour + day * 24]
+
+    # calculate average
+    value_availability = value_availability / 365
+    value_demand = value_demand / 365
+
+    availability_pv_24h.append(value_availability)
+    DemandVal_24h.append(value_demand)
+
+# write both datasets in csv
+with open("../time_series/TS_PVAvail_24h.csv", "w", newline="") as file:
+    writer = csv.writer(file)
+    writer.writerows(extractDigits(availability_pv_24h))
+
+with open("../time_series/TS_Demand_24h.csv", "w", newline="") as file:
+    writer = csv.writer(file)
+    writer.writerows(extractDigits(DemandVal_24h))
